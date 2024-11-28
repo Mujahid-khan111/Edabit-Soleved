bdf3d6e5c99b0cd5effb6e3b5b4ba7e258ca13ed
mujahid-khan111 <khanmujahid7003@gmail.com>
2024-11-29 00:05:50 +0530

    first commit

---
+++ Edabit-Soleved/day1.js
@@ -0,0 +1,20 @@
+// Return the First Element in an Array
+// Create a function that takes an array containing only numbers and return the first element.
+// The first element in an array always has an index of 0.
+// 1 => qus 
+
+function getfirstValue(arr){
+    return arr[0];
+}
+
+
+console.log(getfirstValue([1,2,3]));
+console.log(getfirstValue([80,5,6]));
+console.log(getfirstValue([-500,0,50]));
+
+
+// 2 => qus 
+
+// Pair Management
+// Given two arguments, return an array which contains these two arguments.
+
